b1eae4232d2bba988141c1a730d39a4d18bc96f5
Gabriel <gabriel59kg@gmail.com>
2017-06-10 12:50:37 -0400

    loop through second level elements

--- initial_testing/read_xml_test1.py
+++ initial_testing/read_xml_test1.py
@@ -4,5 +4,26 @@ tree = ET.parse('country_data.xml')
 root = tree.getroot()
 
 print(root.tag)
+print('Root Elements:', len(root))
 for child in root:
     print(child.tag, child.attrib)
+    print(child.tag, 'Elements:',len(child))
+    print(child[0].tag, child[0].text) #Rank
+    print(child[1].tag, child[1].text) #Year
+    print(child[2].tag, child[2].text) #GDPPC
+
+print('*' * 20)
+
+for child in root:
+    print(child.tag, child.attrib)
+    print(child.tag, 'Elements:',len(child))
+    for element in child:
+        print('\t', element.tag, end=' ')
+
+        if len(element.attrib) > 0 :
+            print(element.attrib, end=' ')
+
+        if element.text is not None:
+            print('=', element.text)
+        else:
+            print()
