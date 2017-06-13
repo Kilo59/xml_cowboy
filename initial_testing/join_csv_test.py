1e700d60d1522aa5b1f748f0de9364b22ffae851
Gabriel <gabriel59kg@gmail.com>
2017-06-13 14:47:06 -0400

    Setup dictionary from CSV file using a 'key_field'
    Set one CSV column as the `key_field` and dictionary is setup with the
    keyfield values as the root dictionary keys.

---
+++ initial_testing/join_csv_test.py
@@ -0,0 +1,47 @@
+# join_csv_test.py
+# Protoype the joining of 2 different CSV files based on a common item.
+
+
+import csv
+
+
+############
+# Functions
+############
+
+def return_dictnry_list(filename):
+
+    list_dict1 = []
+
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        print(type(reader))
+        for row in reader:
+            # print(row)
+            list_dict1.append(row)
+
+    return list_dict1
+
+
+# Create 2 item tuple (key_field, value) use to create dictionary
+def setup_keyfield(dictionary_list, key_field):
+    key_value_list = []
+    for dic in dictionary_list:
+        print(key_field, dic[key_field])
+        # new_dictionary[key_field] = dic[key_field]
+        # key_value_list.append((key_field, dic[key_field]))
+        key_value_list.append((dic[key_field], key_field))
+    # print(new_dictionary)
+    print(key_value_list)
+    new_dictionary = dict(key_value_list)
+    print(new_dictionary)
+    return None
+###########
+
+
+file1 = return_dictnry_list('foods.csv')
+file2 = return_dictnry_list('drink.csv')
+
+# print(file2)
+# print(file1)
+print(setup_keyfield(file1, 'person_id'))
