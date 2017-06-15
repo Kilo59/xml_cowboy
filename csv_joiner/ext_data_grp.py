578da97bd2cef86f647a7403e8cba8a2a9533d45
Gabriel <gabriel59kg@gmail.com>
2017-06-15 15:15:28 -0400

    create report obj class

--- csv_joiner/ext_data_grp.py
+++ csv_joiner/ext_data_grp.py
@@ -16,7 +16,7 @@ import copy
 # combined_data_grp  holds 1 or more data_groups and "joins" them on a common field
 class data_grp(object):
     # instiate object
-    def __init__(self, key_field_name, core_filename, suppl_filename=None, excluded_fields=[]):
+    def __init__(self, key_field_name, core_filename, suppl_filename=None, report_fields=['all']):
             self.key_field_name = key_field_name
             self.core_filename = core_filename
             self.suppl_filename = suppl_filename
@@ -29,9 +29,11 @@ class data_grp(object):
             else:
                 self.dictionary = self.data_grp_core.dictionary
                 self.fields = self.data_grp_core.o_fields
-            self.excluded_fields = excluded_fields
-
-            self.report_obj = report_obj(self.get_dictionary(), r_excld_fields=self.excluded_fields)
+            if report_fields[0] == 'all':
+                self.report_fields = self.fields  # use all fields
+            elif len(report_fields) > 1:  # use specified fields
+                self.report_fields = report_fields
+            self.report_obj = report_obj(self.get_dictionary(), self.report_fields)
 
     def __str__(self):
         # return pprint.pformat(self.melded_dictionary)
@@ -69,10 +71,17 @@ class data_grp(object):
         returned_dict = copy.deepcopy(self.dictionary)
         if strip_pkey is True:
             returned_dict.pop('PRIMARY_KEY')
+        # for k in returned_dict.keys():
+        #         if k not in self.report_fields:
+        #             returned_dict.pop(k)
         return returned_dict
 
     def str_dictionary(self, strip_key=True):
         return pprint.pformat(self.get_dictionary(strip_key))
+
+    def make_report(self, report_name='report.csv'):
+        self.report_obj.write_report(report_name)
+        return
 
 
 class data_object(object):
@@ -117,46 +126,55 @@ class data_object(object):
 
 class report_obj(object):
     # instiate object
-    def __init__(self, r_dictionary, report_name=None, r_type='DEFAULT', r_excld_fields=[]):
+    def __init__(self, r_dictionary, r_fields, report_name=None, r_type='DEFAULT', r_excld_fields=[]):
             self.report_name = report_name
             self.r_type = r_type
             self.r_dictionary = r_dictionary
+            self.r_fields = r_fields
             self.r_excld_fields = r_excld_fields
             self.headers = True
 
-    def mergedict(a, b):
+    def mergedict(self, a, b):
         a.update(b)
         return a
+
+    def write_report(self, output_name='report.csv'):
+        with open(output_name, "w") as f:
+            w = csv.DictWriter(f, self.r_fields)
+            w.writeheader()
+            print(self.r_fields)
+            for k, d in sorted(self.r_dictionary.items()):
+                w.writerow(self.mergedict({self.r_fields[0]: k}, d))
+        return None
 ######################
 #  END CLASS DEFINITION
 ######################
 
 
-xdg2 = data_grp('person_id', 'foods.csv', 'drink.csv')
-print(xdg2)
-# xdg1 = data_grp('person_id', 'foods.csv')
-# print(xdg1)
-dict2 = xdg2.dictionary
-print(xdg2.str_dictionary())
-print(xdg2.fields)
+# TODO: allow for fields to be excluded from report
+# TODO: pass report fields in make_report() function not during data_grp() creation
+report_fields2 = ['person_id', 'name', 'fav_food', 'hate_food', 'fav_drink']
+xdg2 = data_grp('person_id', 'foods.csv', 'drink.csv', report_fields=report_fields2)
+xdg2.make_report()
 
+report_fields3 = ['person_id', 'fav_food', 'hate_food', 'fav_drink', 'name']
+xdg3 = data_grp('person_id', 'foods.csv', 'drink.csv', report_fields=report_fields2)
+xdg3.make_report(report_name='name_last.csv')
 ####################
 # CSV output test
 ####################
 
 
-def mergedict(a, b):
-    a.update(b)
-    return a
+# def mergedict(a, b):
+#     a.update(b)
+#     return a
 
 
-header_list = ['person_id', 'name', 'fav_food', 'hate_food', 'fav_drink']
-
-with open("test_output.csv", "w") as f:
-    w = csv.DictWriter(f, header_list)
-    w.writeheader()
-    for k, d in sorted(dict2.items()):
-        if k != 'PRIMARY_KEY':  # TODO: strip this field from dictionary
-            w.writerow(mergedict({header_list[0]: k}, d))
-
-print()
+# header_list = ['person_id', 'name', 'fav_food', 'hate_food', 'fav_drink']
+#
+# with open("test_output.csv", "w") as f:
+#     w = csv.DictWriter(f, header_list)
+#     w.writeheader()
+#     for k, d in sorted(dict2.items()):
+#         if k != 'PRIMARY_KEY':  # TODO: strip this field from dictionary
+#             w.writerow(mergedict({header_list[0]: k}, d))
